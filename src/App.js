d8e61fd32f9b62a6c501424ddcf383df1c001437
Viannn <103623322+Luviandsp2005@users.noreply.github.com>
2024-05-08 00:34:54 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import "./style/homeLogin.css"
 import Login from './pages/LoginPage'
 import Signup from './pages/SignUpPage'
 import Identity from './pages/IdentityPages'
+import Skill from './pages/SkillSelect'
 import Home from './pages/Home'
-import Learning from './pages/Learningpage'
 
 function App() {
   return (
@@ -17,8 +16,8 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/identity' element={<Identity />} />
+          <Route path='/skill' element={<Skill />} />
           <Route path='/home' element={<Home />} />
-          <Route path='/learning' element={<Learning />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/SkillSelect.js
@@ -0,0 +1,61 @@
+import styles from '../style/skill.module.css';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Beginner from '../assets/img/beginner.png';
+import Intermediate from '../assets/img/intermediate.png';
+import Expert from '../assets/img/expert.png';
+import { Link } from 'react-router-dom';
+
+function Skill() {
+    const cardsData = [
+        {
+            title: "Beginner",
+            imageSrc: Beginner,
+            link: "/home"
+        },
+        {
+            title: "Intermediate",
+            imageSrc: Intermediate,
+            link: "/home"
+        },
+        {
+            title: "Expert",
+            imageSrc: Expert,
+            link: "/home"
+        }
+    ];
+
+    return (
+        <div className={styles.container} style={{ margin: 'auto 100px' }}>
+            <div className={styles.header} style={{ backgroundColor: 'white', borderRadius: '20px', height: '70px', maxWidth: '500px', margin: '50px auto' }}>
+                <h1 style={{ textAlign: 'center', marginTop: '10px', fontWeight: 'bolder' }}>
+                    Choose Your Skill Level
+                </h1>
+            </div>
+
+            <Row xs={1} md={3} className="g-0" style={{ display: 'flex', justifyContent: 'center' }}>
+                {cardsData.map((card, idx) => (
+                    <Col key={idx}>
+                        <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                            <Link to={card.link} style={{ textDecoration: 'none' }}>
+                                <Card className={styles.customCard} style={{ borderRadius: '50px', height: '500px', width: '350px' }}>
+                                    <Card.Img variant="top" src={card.imageSrc} className={styles.cardImage} style={{ width: '300px', margin: '30px auto' }}/>
+                                    <Card.Body style={{ textAlign: 'center' }}>
+                                        <Card.Title className={styles.customTitle} style={{ marginTop: idx === 0 ? '20px' : '60px' }}>
+                                            <h1 style={{ fontWeight: 'bolder' }}>
+                                                {card.title}
+                                            </h1>
+                                        </Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
+                        </div>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
+}
+
+export default Skill;
