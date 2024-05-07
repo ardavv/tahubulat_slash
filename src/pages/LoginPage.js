8a8957557cf48156cb2c22ae2e921e9d6dc5f617
Zumi <45333659+zygreion@users.noreply.github.com>
2024-05-07 14:50:48 +0700

    Small details
    - Reduce size of login method image
    - Add padding to login button
    - Change text for "Remember me" checkbox
    - Add dummy logo above login title

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,18 +6,20 @@ import { Link } from "react-router-dom"
 import Facebook from '../assets/img/facebook.svg'
 import Google from '../assets/img/google.svg'
 import Apple from '../assets/img/apple.svg'
+import Upn from '../assets/img/upn.png'
 
 function Login() {
     return (
         <div className='myBG'>
-            <div className='border center-card'>
+            <div className='center-card'>
 
-                <Container className=' text-white d-flex justify-content-center bg border'>
+                <Container className=' text-white d-flex justify-content-center bg'>
                     <Row>
                         <Col>
                             <Card bg='light'>
-                                <div className='title'>Login To</div>
-                                <div className='title'>Continue</div>
+                                <Image src={Upn} className='site-logo' />
+                                <div className='title'>Login</div>
+                                {/* <div className='title'>Continue</div> */}
                                 <Form>
                                     <Form.Group className="mb-3" controlId="formBasicEmail">
                                         <Form.Label className='bold-text'>Your Email:</Form.Label>
@@ -32,7 +34,7 @@ function Login() {
                                         <Form.Control type="password" placeholder="Password" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                        <Form.Check type="checkbox" label="Check me out" />
+                                        <Form.Check type="checkbox" label="Remember me" />
                                     </Form.Group>
 
 
@@ -53,7 +55,7 @@ function Login() {
                                 <div className='link-container'>
                                     <Card.Link href="#" className='card-link'>Forgot Password?</Card.Link>
                                     <Link to='/signup'>
-                                    <Card.Link href="#" className='card-link'>Create an Account!</Card.Link>
+                                    <Card.Link href="#" className='card-link'>I don't have an account</Card.Link>
                                     </Link>
                                 </div>
                             </Card>
