13610af3734d375c00036fe560e8fc3381d5dfd4
Zumi <45333659+zygreion@users.noreply.github.com>
2024-05-08 12:16:16 +0700

    Improvement on Navbar
    - Changing bootstrap NavLink to route Link with image
    - Adding icon parameter to custom nav link
    - Adding nav icons to assets/img/
    - Moving navigation parts of homeLogin.css into new file navigationBar.css

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,28 @@
 import { Link, useLocation } from "react-router-dom"
-import { Navbar, Container, Nav, NavDropdown, NavLink } from "react-bootstrap"
+import { Navbar, Container, Nav, NavDropdown, Image } from "react-bootstrap"
 import "../style/homeLogin.css"
+import "../style/navigationBar.css"
+import HomeIcon from '../assets/img/icon_home.png'
+import LearningIcon from '../assets/img/icon_learning.png'
+import PlaygroundIcon from '../assets/img/icon_playground.svg'
+import CreativeLabIcon from '../assets/img/icon_creativeLab.png'
 
-const CustNavLink = ({ name, goTo }) => {
+const CustNavLink = ({ icon, name, goTo }) => {
     const curPath = useLocation().pathname;
-    return curPath === goTo ? 
-        <Nav.Link id="activeNav" className="active" as={Link} to={goTo}>{name}</Nav.Link> : 
-        <Nav.Link as={Link} to={goTo}>{name}</Nav.Link>;
+    const newId = curPath === goTo? "activeNav" : "";
+    const newClass = curPath === goTo? "active" : "";
+    return (
+        <>
+            <Link id={newId} className={newClass + ' nav-icon-link'} to={goTo}>
+                    <Image className="nav-icon" src={icon}></Image>
+                    <span className="nav-cust-span">{name}</span>
+            </Link>
+        </>
+    )
+}
+
+const ProfileClickHandler = (e) => {
+
 }
 
 const ProfileInHandler = (e) => {
@@ -22,25 +38,32 @@ const ProfileOutHandler = (e) => {
 const NavigationBar = () => {
     return (
         <>
-            <Navbar data-bs-theme="danger" style={{ fontWeight: 'bold', fontSize: 'large' }}>
+            <Navbar data-bs-theme="danger" style={{ fontWeight: 'bold', fontSize: 'larger' }}>
                 <Container>
                     <Navbar.Brand as={Link} to="/home">TahuBulat</Navbar.Brand>
                     <Nav variant="pills">
-                        <CustNavLink name="Home" goTo="/home" />
-                        <CustNavLink name="Learning" goTo="/learning" />
-                        <CustNavLink name="Playground" goTo="#" />
-                        <CustNavLink name="Creative Lab" goTo="#" />
-                        <NavDropdown title="Profile" id="collapsible-nav-dropdown" onFocus={ProfileInHandler} onBlur={ProfileOutHandler}>
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Log Out
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <CustNavLink icon={HomeIcon} name="Home" goTo="/home" />
+                        {/* <Link>
+                            <Image className="nav-icon" src={HomeIcon}></Image>
+                            <br></br>
+                            <span className="nav-cust-span">Tess</span>
+                        </Link> */}
+                        <CustNavLink icon={LearningIcon} name="Learning" goTo="/learning" />
+                        <CustNavLink icon={PlaygroundIcon} name="Playground" goTo="#" />
+                        <CustNavLink icon={CreativeLabIcon} name="Creative Lab" goTo="#" />
+                        <Link className="nav-icon-link">
+                            <NavDropdown title="Profile" id="collapsible-nav-dropdown" onFocus={ProfileInHandler} onBlur={ProfileOutHandler}>
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Log Out
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Link>
                     </Nav>
                 </Container>
             </Navbar>
